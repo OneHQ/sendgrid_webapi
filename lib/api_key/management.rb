b38faa41759c5a7e8dc089e6a8c54ae0bb8f9580
Uriel Pereira <uriel.p@onehq.com>
2021-03-19 16:24:14 -0300

    added on_behalf_of header for apikeys

--- lib/api_key/management.rb
+++ lib/api_key/management.rb
@@ -8,19 +8,26 @@ module SendGridWebApi::ApiKeyModules
         management_get_url += "/#{options[:apikey_id]}"
         options.delete(:apikey_id)
       end
-      query_api(management_get_url, options)
+      query_api(management_get_url, process_header_options(options))
     end
 
     def create options = {}
-      query_post_json_api(APIKEY_URL, options)
+      query_post_json_api(APIKEY_URL, process_header_options(options))
     end
 
-    def delete apikey_id
-      query_delete_api(APIKEY_URL+"/#{apikey_id}", {})
+    def delete apikey_id, options = {}
+      query_delete_api(APIKEY_URL+"/#{apikey_id}", process_header_options(options))
     end
 
     def update apikey_id, options = {}
-      query_patch_api(APIKEY_URL+"/#{apikey_id}", options)
+      query_patch_api(APIKEY_URL+"/#{apikey_id}", process_header_options(options))
+    end
+
+    private
+    def process_header_options options
+      on_behalf_of = options.delete(:on_behalf_of)
+      options.merge!({ headers: { "on-behalf-of" => on_behalf_of } }) unless on_behalf_of.nil?
+      options
     end
   end
 end
